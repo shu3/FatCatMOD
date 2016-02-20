28e535375bae6484cc71b23eec642a4561852023
shu3 <shu3321@gmail.com>
2016-02-20 19:35:45 +0900

    Revert "便意MAX時に餌をあげた時にダメージを受けないように修正"
    This reverts commit 30180b18c190691c7db8d2cf0d19585c4e8e43ce.

--- src/main/java/fatcat/EntityFatCat.java
+++ src/main/java/fatcat/EntityFatCat.java
@@ -515,6 +515,7 @@ public class EntityFatCat extends EntityTameable {
 
 		// 便意MAX以上だとダメージ
 		if (bladder > 100) {
+			attackEntityFrom(DamageSource.generic, 5.0F);
 			bladder = 100;
 		}
 		if (bladder < 0) {
