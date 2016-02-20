0fb5d03863ed5c87e1ab03a9bfd49423158ed9fb
shu3 <shu3321@gmail.com>
2016-02-20 19:41:31 +0900

    version 1.0.3

--- src/main/java/fatcat/FatCatMod.java
+++ src/main/java/fatcat/FatCatMod.java
@@ -52,7 +52,7 @@ import fatcat.gui.GuiStatusHandler;
 @Mod(modid = FatCatMod.MODID, version = FatCatMod.VERSION)
 public class FatCatMod {
     public static final String MODID = "fatcat";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.0.3";
 	public static final int STATUS_GUI_ID = 0;
     public static Item egg;
     public static Item unko;
